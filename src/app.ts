03f11c6d19e1831cc769a9b27fdb3eb3e1a4bfb5
Vlatko Rohak <vlatko.rohak@eccos.com.hr>
2024-10-20 01:53:13 +0200

    env usage

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,23 @@
-import express, { Application, Express, Request, Response, NextFunction } from "express";
+import express, {
+  Application,
+  Express,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
 import dotenv from "dotenv";
 import ticketRoutes from "./routes/ticketRoutes";
 import { auth } from "express-oauth2-jwt-bearer";
 
 dotenv.config();
 
-// const jwt = require("express-jwt");
-
 const app: Application = express();
 
 const jwtCheck = auth({
-  issuerBaseURL: 'https://dev-1xw25n8drb5r4asm.eu.auth0.com/',
-    audience: 'https://web2-lab1-87jt.onrender.com'
-  });
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  audience: process.env.RESOURCE_SERVER,
+});
 
-// const jwtCheck = jwt({
-//   secret: getPublicKey(process.env.AUTH0_DOMAIN),
-//   audience: process.env.RESOURCE_SERVER,
-//   algorithms: ["RS256"],
-//   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-// });
 
 app.use(jwtCheck);
 
@@ -35,15 +33,12 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err) {
     if (err.status === 401) {
       res.status(401).json({ message: err.message });
+    } else {
+      res.status(500).json({ message: "An unexpected error occurred" });
     }
-    else{
-        res.status(500).json({ message: "An unexpected error occurred" });
-    }
-  }
-  else{
+  } else {
     next(err);
   }
-  
 });
 
 const PORT = process.env.PORT || 8000;
